151b25efff1e98593f75ee91343d2b56f9488f48
Eshwanth Karti T R <eshwanthkartitr@gmail.com>
2024-10-04 09:03:45 +0530

    Fisst

--- scripts/themeSetup.js
+++ scripts/themeSetup.js
@@ -123,3 +123,13 @@ const setupTheme = () => {
 };
 
 setupTheme();
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
